cebf9e44b9d9d2660d45a5e53f7b652bf3f483ad
Lupita Estrada <kim1882@gmail.com>
2024-02-01 10:43:51 -0600

    add env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,33 @@ import { RoomTypesModule } from './room_types/room_types.module';
 import { RoomType } from './room_types/entities/room_type.entity';
 import { BookingsModule } from './bookings/bookings.module';
 import { Booking } from './bookings/entities/booking.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: 'postgres',
-      username: 'postgres',
-      entities: [Room, RoomType, Booking],
-      database: 'hotelMgmt',
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: 5432,
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        username: configService.get<string>('POSTGRES_USER'),
+        entities: [Room, RoomType, Booking],
+        database: configService.get<string>('POSTGRES_DATABASE'),
+        synchronize: true,
+        logging: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }),
     }),
     RoomsModule,
     RoomTypesModule,
