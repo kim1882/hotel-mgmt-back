884e8a1c633b0d595ed064e7fece6736a837b492
Lupita Estrada <kim1882@gmail.com>
2024-02-02 20:48:18 -0600

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS' });
+
+  const config = new DocumentBuilder()
+    .setTitle('Hotel Management API')
+    .setDescription('CRUD operations to manage room types, rooms and bookings')
+    .setVersion('1.0')
+    .addTag('room types')
+    .addTag('rooms')
+    .addTag('bookings')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
